c20dc809eef095129211b18b7ff0415f9c426057
Allan5829 <allan.farca@gmail.com>
2021-02-09 21:46:04 -0800

    finished and passes tests for addition portion.

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 function additivePersistence(n) {
     let result = 0;
-    let string = n.toString().split("")
+
+    // Single-Diget Value
     if ( n < 10 && n > 0) {
-        console.log("yes")
+        return result
     }
-	console.log(n, string)
+    
+    // Multiple-Diget Value
+    while (n > 9) {
+       let string = n.toString().split("")
+        n = 0;
+        string.forEach( x => { // using reduce yielded an odd error where x became 1,1,2,4,8,16...
+            n += parseInt(x, 10)
+        })
+        result++
+        if ( n < 10 && n > 0) {
+            console.log(result)
+            return result
+        } 
+    }
 }
 
 function multiplicativePersistence(n) {
 	
 }
 
-additivePersistence(1679583)
+additivePersistence(199)
 
 /*
 pseudo code
 create what 1 line of a loop would look like and turn it into a while loop using the if condition above
+
+example 
+additivePersistence(1679583) ➞ 3
+// 1 + 6 + 7 + 9 + 5 + 8 + 3 = 39
+// 3 + 9 = 12
+// 1 + 2 = 3
+// It takes 3 iterations to reach a single-digit number.
 */
