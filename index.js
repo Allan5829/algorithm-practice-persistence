4bfb4cee4c2e76d1d5872eaeea17b9f5dae38d04
Allan5829 <allan.farca@gmail.com>
2021-02-09 21:21:58 -0800

    writes pseudo code comments and writes some logic for future loop.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function additivePersistence(n) {
-	
+    let result = 0;
+    let string = n.toString().split("")
+    if ( n < 10 && n > 0) {
+        console.log("yes")
+    }
+	console.log(n, string)
 }
 
 function multiplicativePersistence(n) {
 	
 }
+
+additivePersistence(1679583)
+
+/*
+pseudo code
+create what 1 line of a loop would look like and turn it into a while loop using the if condition above
+*/
